fe7065c9433338d480052eca11ec2419550c5605
Mattyou777 <mattyou777@gmail.com>
2018-02-08 04:25:46 +0300

    Add counterclockwise method

--- src/com/epam/training/printArray/printArray.java
+++ src/com/epam/training/printArray/printArray.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by Matt on 08.02.2018.
  */
 public class printArray {
-
+    //prints from the top left corner clockwise
     public static void print(int [][] arr){
         int mini=0;
         int minj=-1;
@@ -33,6 +33,40 @@ public class printArray {
             }
             for (int i=--maxi;i>=mini;i--) {
                 list.add(arr[i][minj] + " ");
+                if(list.size()==size)
+                    break main;
+            }
+        }
+        list.forEach(System.out::print);
+
+    }
+//prints from the top right corner counterclockwise
+    public static void printRev(int [][] arr){
+        int mini=0;
+        int minj=0;
+        int maxi=arr.length-1;
+        int maxj=arr[0].length;
+        int size=arr.length*arr[0].length;
+        ArrayList list = new ArrayList(arr.length*arr[0].length);
+        main:
+        while (size!=0){
+            for (int j=--maxj;j>=minj;j--){
+                list.add(arr[mini][j] + " ");
+                if(list.size()==size)
+                    break main;
+            }
+            for (int i=++mini;i<=maxi;i++){
+                list.add(arr[i][minj] + " ");
+                if(list.size()==size)
+                    break main;
+            }
+            for (int j=++minj;j<=maxj;j++){
+                list.add(arr[maxi][j] + " ");
+                if(list.size()==size)
+                    break main;
+            }
+            for (int i=--maxi;i>=mini;i--) {
+                list.add(arr[i][maxj] + " ");
                 if(list.size()==size)
                     break main;
             }
