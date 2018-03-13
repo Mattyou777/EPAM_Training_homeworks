413b02ef146725d7f29d90298773c575bfeef8d8
Mattyou777 <35892446+Mattyou777@users.noreply.github.com>
2018-03-13 17:45:08 +0300

    Create Solution.java

---
+++ src/com/epam/training/hackerrank/30_Days_of_Code_Challenges/Day 0- Hello- World/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+		String inputString = scan.nextLine(); 
+		scan.close(); 
+		System.out.println("Hello, World.");
+	}
+}
