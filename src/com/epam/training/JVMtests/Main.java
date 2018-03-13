901d59d46c778c744209f08d5a0ba044437ecf35
Matt <mattyou777@gmail.com>
2018-03-13 17:30:38 +0300

    Adding app

---
+++ src/com/epam/training/JVMtests/Main.java
@@ -0,0 +1,17 @@
+package com.epam.training.JVMtest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Object> list = new ArrayList<Object>();
+        while (true) {
+            list.add(new Object());
+        }
+
+    }
+
+}
