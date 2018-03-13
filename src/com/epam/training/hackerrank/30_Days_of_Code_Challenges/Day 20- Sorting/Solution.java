7e37c756797e7d87474c9db518fa1285d555ae80
Mattyou777 <35892446+Mattyou777@users.noreply.github.com>
2018-03-13 20:12:05 +0300

    Create Solution.java

---
+++ src/com/epam/training/hackerrank/30_Days_of_Code_Challenges/Day 20- Sorting/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Solution {
+    static int numberOfSwaps = 0;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+
+            int temp = 0;
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        System.out.println("Array is sorted in " + numberOfSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n - 1]);
+
+    }
+}
