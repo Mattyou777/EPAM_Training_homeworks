67b5b90e7efd5d9ade3c38d8e1ce376989fa270b
Mattyou777 <35892446+Mattyou777@users.noreply.github.com>
2018-03-13 19:09:40 +0300

    Create Calculator.java

---
+++ src/com/epam/training/hackerrank/30_Days_of_Code_Challenges/Day 17- More Exceptions/Calculator.java
@@ -0,0 +1,8 @@
+class Calculator {
+    int power(int a, int b) throws Exception {
+        if (a >= 0 && b >= 0)
+            return (int) Math.pow(a, b);
+        else
+            throw new Exception("n and p should be non-negative");
+    }
+}
